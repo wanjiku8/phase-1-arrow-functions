0c5ad84731919af0a5b855c05111af7a92475fea
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-01 02:36:32 +0300

    commit

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const add = (a, b) => a + b;
-
-function multiplyNumbers(a, b) {
-    return a * b;
+const foo = function() {
+    return 'bar';
   }
-
-  const multiplyNumbers = (a, b) => a * b;
-
-
+  foo();
+  
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+  console.log(add(2, 3));
+  
+  const twoAdder = x => x + 2;
+  console.log(twoAdder(3));
+  
+  const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  }
+  console.log(sum(1, 2));
+  
   const nums = [1, 2, 3];
-  const squares = nums.map(num => num ** 2);
-  console.log(squares); 
-    
+  const squares = nums.map(x => x ** 2);
+  console.log(squares);
+  
+  const finishedItems = overdueTodoItems.map(item => item.className = "complete");
+  header.innerText = `You finished ${finishedItems.length} items!`;
+  
+  lapsedUserAccounts.map(u => sendBillTo(u.address));
+  
