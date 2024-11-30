bb7f55f56fb315db1ed9994d96b9f80fb835e543
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-01 02:21:27 +0300

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const add = (a, b) => a + b;
+
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  const multiply = (a, b) => a * b;
+
+  const nums = [1, 2, 3];
+  const squares = nums.map(num => num ** 2);
+  console.log(squares); 
+    
